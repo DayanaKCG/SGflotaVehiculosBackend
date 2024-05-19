a0f861e3e96e2d12f94c967b351917ebb713ea10
DayanaKCG <dcordoba3341@campuscecep.edu.co>
2024-05-19 15:42:01 -0500

    migracion de conductores

--- database/migrations/2024_05_19_203713_create_conductores_table.php
+++ database/migrations/2024_05_19_203713_create_conductores_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('conductores', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('licencia');
+            $table->date('fecha_contratacion');
             $table->timestamps();
         });
     }
